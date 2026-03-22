f5f41530329bf2848816e2ce97aa6a045f59642b
SariinVtiin <xvitorc.fernandes@gmail.com>
2026-03-22 14:43:56 -0700

    TESTE

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,10 +3,10 @@ const mysql = require("mysql2/promise");
 
 // Pool de conexões para melhor performance
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "root",
-  database: process.env.DB_DATABASE || "etiquetas_hospital",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
